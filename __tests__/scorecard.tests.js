7b18562ca61e2f9a2864e0081b743b795fb8a018
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-07-17 21:21:00 +0100

    bowling hell

--- __tests__/scorecard.tests.js
+++ __tests__/scorecard.tests.js
@@ -6,8 +6,53 @@ describe('scorecard', () => {
     scorecard = new Scorecard();
   });
   describe('constructor', () => {
+
     it('returns an object', () => {
       expect(scorecard).toBeInstanceOf(Object);
+    });
+
+    it('starts with a score of 0', () => {
+      expect(scorecard.totalScore).toEqual(0)
+    });
+    
+    it('frame is an array', () => {
+    expect(scorecard.score).toBeInstanceOf(Array)
+    });
+
+    it('adds frame score to frame array', () => {
+      scorecard.bowl(4);
+    expect(scorecard.score).toEqual([4])
+    });
+
+    it('contains multiple bowls in the score array', () => {
+      scorecard.bowl(4);
+      scorecard.bowl(5);
+      expect(scorecard.score).toEqual([4,5])
+
+    })
+
+    it('bowl function increases the frame index by 1', () => {
+     scorecard.bowl(6);
+    expect(scorecard.frameIndex).toEqual(1);
+    });
+
+    it('if frame index is equal to 10, your game is finished', () => {
+      scorecard.frameIndex = 9;
+      scorecard.bowl(5);
+      expect(scorecard.gameStatus).toBe('Your game is finished');
+    });
+
+    it('get total score', () => {
+      scorecard.bowl(5);
+      scorecard.bowl(5);
+      scorecard.calculateTotalScore();
+      expect(scorecard.totalScore).toBe(10);
+    })
+});
+  describe('detect strike method', () => {
+    it('detects a strike', () => {
+      scorecard.detectStrike
+    expect
     });
   });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,7 +1,27 @@
 class Scorecard {
   constructor() {
-    
+    this.totalScore = 0;
+    this.score = [];
+    this.frameIndex = 0;
+    this.gameStatus = 0;
+    };
+
+  bowl(pins) {
+    this.score.push(pins);
+    if (this.frameIndex === 10) {
+      this.gameStatus = 'Your game is finished'
+  };
+
+};
+  calculateTotalScore() {
+    this.totalScore = this.calculateFrame();
+  };
+
+  calculateFrame() {
+    console.log(this.score)
+    return this.score[this.frameIndex] + this.score[this.frameIndex + 1];
   }
+  
 };
 
 module.exports = Scorecard;
